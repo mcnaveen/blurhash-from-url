75c335254b77847b30b997e30887255b88f1564d
mcnaveen <8493007+mcnaveen@users.noreply.github.com>
2022-12-15 10:53:51 +0530

    refactor(core): :recycle: Better error handling with Promise based function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 import { encode, decode } from "blurhash";
-import sharp from 'sharp';
+import sharp from "sharp";
 
 export interface IOutput {
     encoded: string;
@@ -9,32 +9,53 @@ export interface IOutput {
     height: number;
 }
 
-export const blurhashFromURL = async (url: string) => {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    const returnedBuffer = Buffer.from(arrayBuffer);
+export const blurhashFromURL = (url: string): Promise<IOutput> => {
+    return new Promise((resolve, reject) => {
+        try {
+            fetch(url)
+                .then((response) => response.arrayBuffer())
+                .then((arrayBuffer) => {
+                    const returnedBuffer = Buffer.from(arrayBuffer);
 
-    const { data, info } = await sharp(returnedBuffer)
-        .ensureAlpha()
-        .raw()
-        .toBuffer({
-            resolveWithObject: true,
-        });
-    const encoded = encode(
-        new Uint8ClampedArray(data),
-        info.width,
-        info.height,
-        4,
-        4
-    );
-    const decoded = decode(encoded, info.width, info.height);
+                    return sharp(returnedBuffer)
+                        .ensureAlpha()
+                        .raw()
+                        .toBuffer({
+                            resolveWithObject: true,
+                        });
+                })
+                .then(({ data, info }) => {
+                    const encoded = encode(
+                        new Uint8ClampedArray(data),
+                        info.width,
+                        info.height,
+                        4,
+                        4
+                    );
+                    const decoded = decode(encoded, info.width, info.height);
 
-    const output: IOutput = {
-        encoded: encoded,
-        decoded: decoded,
-        width: info.width,
-        height: info.height,
-    };
+                    const output: IOutput = {
+                        encoded: encoded,
+                        decoded: decoded,
+                        width: info.width,
+                        height: info.height,
+                    };
 
-    return output;
+                    resolve(output);
+                })
+                .catch((error) => {
+                    reject(
+                        new Error(
+                            `Failed to generate blurhash from URL: ${error.message}`
+                        )
+                    );
+                });
+        } catch (error) {
+            reject(
+                new Error(
+                    `Failed to generate blurhash from URL: ${error.message}`
+                )
+            );
+        }
+    });
 };
--- test/index.js
+++ test/index.js
@@ -1,8 +1,37 @@
 const { blurhashFromURL } = require("../dist/index.js");
 
 async function getBlurhash() {
-  const output = await blurhashFromURL("https://i.imgur.com/NhfEdg2.png");
-  console.log(output);
+  try {
+    const output = await blurhashFromURL("https://i.imgur.com/NhfEdg2.png");
+    console.log(output);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function getBlurhashPromise() {
+  return new Promise((resolve, reject) => {
+    blurhashFromURL("https://i.imgur.com/NhfEdg2.png")
+      .then((output) => {
+        console.log(output);
+        resolve();
+      })
+      .catch((error) => {
+        console.error(error.message);
+        reject(error);
+      });
+  });
+}
+
+async function getBlurhashError() {
+  try {
+    const output = await blurhashFromURL("https://example.com");
+    console.log(output);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 getBlurhash();
+getBlurhashPromise();
+getBlurhashError();
